bb31b28c4ef727733af5f871b0c61a3a562da9b0
Juancho <crackmilo29304@gmail.com>
2026-07-06 11:42:40 -0500

    feat(init): initialize spring project and db connection

---
+++ app/src/test/java/com/medicore/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicore.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
