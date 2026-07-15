f31fbf52ef183e13be048893d5ab34e184be43da
Juancho <crackmilo29304@gmail.com>
2026-07-14 21:15:18 -0500

    fix: add spring label repository at UserRepository

--- app/src/main/java/com/medicore/app/repository/UserRepository.java
+++ app/src/main/java/com/medicore/app/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.medicore.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.medicore.app.models.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     public User findByDocumentNumberAndPassword(String documentNumber, String password);
 }
