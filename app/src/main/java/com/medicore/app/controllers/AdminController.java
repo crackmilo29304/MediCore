731e6c17aae16bc19ab1e215409e89dc88f4a8d7
Juancho <crackmilo29304@gmail.com>
2026-08-25 07:02:29 -0500

    feature(build-views): Add all admin views for user management and stock
    admin menu view was rebuild

--- app/src/main/java/com/medicore/app/controllers/AdminController.java
+++ app/src/main/java/com/medicore/app/controllers/AdminController.java
@@ -9,4 +9,22 @@ public class AdminController {
     public String showAdminMenu() {
         return "admin/adminMenu"; // Busca home.html en templates/
     }
+    @GetMapping("/admin/registerEmployees")
+    public String showRegisterEmployeesView() {
+        return "admin/registerEmployees"; // Busca registerEmployees.html en templates/
+    }
+
+    @GetMapping("/admin/searchEmployees")
+    public String showSearchEmployeesView() {
+        return "admin/searchEmployees"; // Busca searchEmployees.html en templates/
+    }
+
+    @GetMapping("/admin/medicineStock")
+    public String showMedicineStockView() {
+        return "admin/medicineStock"; // Busca medicinesStock.html en templates/
+    }
+    @GetMapping("/admin/deleteUsers")
+    public String showDeleteUsersView() {
+        return "admin/deleteUsers"; // Busca deleteUsers.html en templates/
+    }
 }
