8ab129059671c9af4ecc70629fcb9eaed329811a
Juancho <crackmilo29304@gmail.com>
2026-08-26 17:02:03 -0500

    feature(build-views): Add all patient views for scheduling, canceling appointments, viewing branches, and managing personal data

--- app/src/main/java/com/medicore/app/controllers/PatientController.java
+++ app/src/main/java/com/medicore/app/controllers/PatientController.java
@@ -9,4 +9,29 @@ public class PatientController {
     public String showPatientMenu() {
         return "patient/patientMenu"; // Busca home.html en templates/
     }
+    @GetMapping("/patient/updatePersonalData")
+    public String showUpdatePersonalDataView() {
+        return "patient/updatePersonalData"; // Busca updatePersonalData.html en templates/
+    }
+    @GetMapping("/patient/activePrescriptions")
+    public String showActivePrescriptionsView() {
+        return "patient/activePrescriptions"; // Busca activePrescriptions.html en templates/
+    }
+    
+    @GetMapping("/patient/pqrs")
+    public String showPQRSView() {
+        return "patient/pqrs"; // Busca pqrs.html en templates/
+    }
+    @GetMapping("/patient/branches")
+    public String showBranchesView() {
+        return "patient/branches"; // Busca branches.html en templates/
+    }
+    @GetMapping("/patient/appointments/schedule")
+    public String showScheduleView() {
+        return "patient/appointments/schedule"; // Busca schedule.html en templates/
+    }
+    @GetMapping("/patient/appointments/cancel")
+    public String showCancelView() {
+        return "patient/appointments/cancel"; // Busca cancel.html en templates/
+    }
 }
